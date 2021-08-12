8f74780e076187f9ab2a4fa3341f1cec87e8c62b
rushika1503 <rsirigadde_c@rpxcorp.com>
2021-08-12 19:43:25 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import {useSelector} from "react-redux";
+import {selectUser} from "./features/userSlice";
+import React from 'react';
+
+const App= () => {
+  const user = useSelector(selectUser);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? <Logout/> : <Login/>}
+    
+
     </div>
   );
 }
